f8385a3b4a6be98841f378248cd4692301e9a055
Ashif Iqbal <ashif.iqbal@metropolia.fi>
2021-05-09 07:17:26 +0300

    final

--- src/components/DetailCard.js
+++ src/components/DetailCard.js
@@ -7,21 +7,23 @@ const DetailCard = ({ post }) => {
   const company = post.company.name;
   const classes = style();
   return (
-    <div className={classes.card}>
-      <div className={classes.info}>
-        <h3>Name: {name}</h3>
-        <p>UserName: {username}</p>
-        <p>Email: {email}</p>
-        <p>Phone: {phone}</p>
-        <p>Company: {company}</p>
-        <a href={`https://${website}`} target='_blank' rel='noreferrer'>
-          www.{website}
-        </a>
-        <h4> Address: </h4>
-        <p>Street: {street}</p>
-        <p>Suite: {suite}</p>
-        <p>City: {city}</p>
-        <p>Zipcode: {zipcode}</p>
+    <div className={classes.container}>
+      <div className={classes.card}>
+        <div className={classes.info}>
+          <h3>Name: {name}</h3>
+          <p>UserName: {username}</p>
+          <p>Email: {email}</p>
+          <p>Phone: {phone}</p>
+          <p>Company: {company}</p>
+          <a href={`https://${website}`} target='_blank' rel='noreferrer'>
+            www.{website}
+          </a>
+          <h4> Address: </h4>
+          <p>Street: {street}</p>
+          <p>Suite: {suite}</p>
+          <p>City: {city}</p>
+          <p>Zipcode: {zipcode}</p>
+        </div>
       </div>
     </div>
   );
@@ -32,14 +34,18 @@ DetailCard.propTypes = {
 };
 
 const style = makeStyles({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  },
   card: {
-    height: 400,
     width: 400,
     background: 'lightblue',
     borderRadius: '10px',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     textAlign: 'center',
-    marginLeft: 500,
   },
 });
 export default DetailCard;
